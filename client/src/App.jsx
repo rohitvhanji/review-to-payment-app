d1f0a21bced86777fa7f84900ac5dffb04a9b4a6
rohitvhanji <56556783+rohitvhanji@users.noreply.github.com>
2025-05-21 17:56:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import QRCode from 'react-qr-code';
 import './index.css';
 
-const GOOGLE_REVIEW_URL = "https://g.co/kgs/jEdHyuL"; // Your Google review link
+const GOOGLE_REVIEW_URL = "https://www.google.com/search?rlz=1C5GCCM_en___IN1147&sca_esv=b97c0088c4eec1a1&cs=1&si=APYL9bs7Hg2KMLB-4tSoTdxuOx8BdRvHbByC_AuVpNyh0x2KzQON-usm7ehNRpdkdqvhOZKKPSstjoKtQ2CnFy0Vt1Y7LBMuPBHBWzJdz36Pvc6uTC5IeAjE6vU3riloyClqFMzroMBObusigP17WT2VIugZpgSrDQ%3D%3D&q=Dr.+Dhanale+Dental+Care+Reviews&sa=X&ved=2ahUKEwjq9ousx7SNAxXQ-TgGHao5NrsQ0bkNegQIHxAD&biw=1382&bih=793&dpr=2.5#lrd=0x3bc101acc68de19d:0x71178828d31100c6,3,,,,"; // Your Google review link
 const PAYMENT_LINK = "upi://pay?pa=dhanaledental@upi&pn=DhanaleDentalCare"; // Replace with real UPI or Stripe link
 
 export default function App() {
