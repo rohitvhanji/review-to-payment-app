67477f438240fb76767b77069329c6135d4f4b02
rohitvhanji <56556783+rohitvhanji@users.noreply.github.com>
2025-05-21 20:06:51 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import AdminLogin from './AdminLogin';
-import AdminDashboard from './AdminDashboard';
-import './index.css';
+import './index.css';  // import your global CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
